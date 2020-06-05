3471b19e452ad0aa258f85ed0c0f56ed41caa336
Chuck Deerinck <cjd9@me.com>
2020-06-05 09:15:11 -0700

    Initial Commit

---
+++ IGC Stats/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IGC Stats
+//
+//  Created by Chuck Deerinck on 6/5/20.
+//  Copyright © 2020 Chuck Deerinck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
